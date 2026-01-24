aee901b87cd1d1d1aa9cf0a02f019d2ae802f599
Prince Singh <princesingh005958@gmail.com>
2026-01-24 22:57:26 +0530

    Add project files.

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DbInitializer
+    {
+        public void InitDB(WebApplication app) 
+        {
+            using var scope = app.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<StoreContext>()
+                ?? throw new InvalidOperationException("Failed to retrieve store context");
+            SeedData(context);
+        }
+        private void SeedData(StoreContext context)
+        {
+            context.Database.Migrate();
+            if (context.Products.Any()) return;
+        }
+    }
+}
